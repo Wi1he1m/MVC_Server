39bed97684a082f45143d585e7de0c589cc60245
William <sirwill20@hotmail.com>
2018-04-13 01:44:24 +0000

    Add login form and handling middleware.

--- app.js
+++ app.js
@@ -128,6 +128,18 @@ app.post("/register", function(req, res){
     });
 });
 
+// LOGIN FORM
+app.get("/login", function(req, res){
+    res.render("login");
+});
+// LOGIN HANDLING
+app.post("/login", passport.authenticate("local",
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+    }), function(req, res){
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server up and running!");
 });
